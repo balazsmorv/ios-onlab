86a7f165f4bfb57e038311df054e363d5dc4bab6
Morvay Balázs <balazsmorvay@yahoo.com>
2020-03-09 21:55:15 +0100

    Initial Commit

---
+++ ios-onlab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-onlab
+//
+//  Created by Morvay Balázs on 2020. 03. 09..
+//  Copyright © 2020. BME AUT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
